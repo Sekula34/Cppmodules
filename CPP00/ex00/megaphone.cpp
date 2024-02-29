cf3522c4fa07e9640ebfced5d88b7e39ba83cee2
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-02-29 19:04:12 +0100

    megaphone

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <string>
 
-int main()
+class Megaphone
 {
-	std::cout << "Just testing make file" << std::endl;
+	public :
+		void yell(std::string sentence);
+};
+
+void Megaphone::yell(std::string sentence)
+{
+	char letter;
+	std::string yelling;
+	
+	if (sentence.empty() == true)
+		yelling = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else 
+	{
+		for(unsigned long i = 0; i < sentence.length(); i++)
+		{
+			letter = std::toupper(sentence[i]);
+			yelling.append(1, letter);
+		}
+	}
+	std::cout << yelling;
+}
+int main(int argc, char **argv)
+{
+	Megaphone my_megaphone;
+	std::string input;
+	
+	if(argc == 1)
+		my_megaphone.yell(input);
+	else
+	{
+		for (int i = 1; i < argc ; i++)
+			my_megaphone.yell(argv[i]);
+	}
+	std::cout << std::endl;
 	return(0);
 }
