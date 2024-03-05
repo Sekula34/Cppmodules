9a42a4db9f387ed63cc7fd6bb38364d91d463e5d
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-03-05 09:47:52 +0100

    references are just easier pointers

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string variable("HI THIS IS variable");
+	std::string* stringPTR(&variable);
+	std::string& stringREF(variable);
+	
+	std::cout << "Memory address of string variable " << &variable << std::endl;
+	std::cout << "Memory address held by stringPTR " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF " << &stringREF << std::endl;
+
+	std::cout<<std::endl;
+
+	std::cout << "The value of the string variable " << variable << std::endl;
+	std::cout << "The value pointed to by stringPTR " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF " << stringREF << std::endl;
+	return (0);
+}
