938fa1dfc5509c722cd1b889a127f1a7591dff4b
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-03-11 16:05:35 +0100

    fixed point here wer go

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	_fixedPointNumValue = 0;
+}
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+Fixed& Fixed::operator= (const Fixed& source)
+{
+	std::cout << "Copy assigment operator called" << std::endl;
+	this->_fixedPointNumValue = source.getRawBits();
+	return (*this);
+}
+
+Fixed::Fixed(const Fixed& source)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = source;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	int rawBits;
+	rawBits = _fixedPointNumValue;
+	return (rawBits);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_fixedPointNumValue = raw;
+}
---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include<iostream>
+
+class Fixed
+{
+	private:
+		int _fixedPointNumValue;
+		static const int _numberOfFractionalBits;
+	public:
+		Fixed();
+		Fixed(const Fixed &source);
+		Fixed& operator= (const Fixed &source);
+		~Fixed();
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+#endif
