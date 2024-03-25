612e7ea9f56ea2d2c6a2cfbae3832f579677f81b
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-03-25 19:13:50 +0100

    more test in main

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -57,31 +57,8 @@ void subjectTest()
 
 int main()
 {
+	test1();
+	test2();
 	subjectTest();
-	// test1();
-	// test2();
-	// Dog basic;
-	// {
-	// Dog tmp = basic;
-	// }
-
-	// Cat first;
-	// {
-	// 	Cat copy(first);
-	// 	Cat assign = first;
-	// }
-	// Animal* animal[10];
-
-	// for(int i = 0; i < 10; i++)
-	// {
-	// 	if(i < 5)
-	// 		animal[i] = new Dog();
-	// 	else
-	// 		animal[i] = new Cat();
-	// }
-	// for(int i = 0; i < 10; i++)
-	// {
-	// 	delete animal[i];
-	// }
 	 return 0;
 }
