d90c50bc2ae195a881fb658081b74e2ef96d2c37
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-03-26 19:19:59 +0100

    hej

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,48 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+# include <exception>
+# include <ostream>
+# include <string>
+# include "Bureaucrat.hpp"
+
+class Form 
+{
+	private :
+		const std::string _name;
+		bool _isSigned;
+		const int _gradeToSign;
+		const int _greadeToExecute;
+
+	public :
+
+		//orthodox
+		Form();
+		Form(const Form& source);
+		Form& operator=(const Form& source);
+		~Form();
+
+		//getters for all attributes
+		std::string getName() const;
+		bool getIsSgined() const;
+		int getGradeToSign() const;
+		int getGradeToExecute() const;
+
+		//subjectfunctions
+		void beSigned(const Bureaucrat& biro);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public :
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& forma);
+
+#endif
