1dc5aa63f6b96d16ee998cbf3d32474879036509
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-03-26 18:47:58 +0100

    First push of ex01

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+#include <exception>
+# include <string>
+
+class Bureaucrat 
+{
+	private :
+		const std::string	_name;
+		int					_grade;
+
+	public:
+
+		Bureaucrat(std::string name, int grade);
+		//orthodox
+		Bureaucrat(void);
+		Bureaucrat(const Bureaucrat& source);
+		Bureaucrat& operator=(const Bureaucrat& source);
+		~Bureaucrat();
+
+		std::string getName() const;
+		int getGrade() const;
+
+		void increaseGrade(void);
+		void increaseGrade(int increment);
+		void decreaseGrade(void);
+		void decreaseGrade(int decrement);
+
+		class GradeTooHightException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& birokrat);
+
+#endif
---
+++ cpp05/ex01/main.cpp
@@ -0,0 +1,96 @@
+#include "Bureaucrat.hpp"
+#include <exception>
+#include <iostream>
+#include <ostream>
+
+//too high
+void test1()
+{
+	try 
+	{
+		Bureaucrat a("Ime", 167);
+	}
+	catch (std::exception &e)
+	{
+		std::cout <<e.what() << std::endl;;
+		throw ;
+	}
+}
+
+//too low
+void test2()
+{
+	try 
+	{
+		Bureaucrat b("Filip", -67);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		throw ;
+	}
+}
+
+//geter functions
+
+void test3()
+{
+	Bureaucrat biro;
+	std::cout << "My name is " << biro.getName() << std::endl;
+	std::cout << "My grade is " << biro.getGrade() << std::endl;
+	//error throw in this case
+
+	// Bureaucrat filip("filip", 70000);
+	// std::cout << "My name is "<< filip.getName() << std::endl;
+	// std::cout << "My grade is " << filip.getGrade() << std::endl;
+}
+
+
+//decrease grade exception and << overload
+void test4()
+{
+	Bureaucrat filip("filip", 150);
+	std::cout << filip << std::endl;
+	filip.decreaseGrade();
+	std::cout << "This will not be printed if exception is thrown" << std::endl;
+}
+
+//increase grade exception and << overload
+void test5()
+{
+	Bureaucrat filip("filip", 1);
+	std::cout << filip << std::endl;
+	filip.increaseGrade();
+	std::cout << "This will not be printed if exception is thrown" << std::endl;
+}
+
+//increase and decrease overload
+void test6()
+{
+	Bureaucrat filip("filip", 12);
+	filip.decreaseGrade(5);
+	std::cout << filip << std::endl;
+	//filip.decreaseGrade(200);
+	filip.increaseGrade(19);
+}
+
+int main()
+{
+	try
+	{
+		//test1();
+		// test2();
+		//test3();
+		//test4();
+		//test5();
+		test6();
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+	std::cout << "Something after main" << std::endl;
+	std::cout << "Everything is good" << std::endl; 
+	return 0;
+}
