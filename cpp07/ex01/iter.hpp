9f35480d88222bf5d6781b0c5d604e54bbdb40df
filip <filip.seles@gmail.com>
2024-04-10 15:14:57 +0200

    thanks arthur for warning about this one

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,24 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+#include <cstddef>
+
+// template<class T, class V>
+// void iter(T* address, const std::size_t length, V(*function)(T& element))
+// {
+// 	for(std::size_t i = 0; i < length; i++)
+// 	{
+// 		function(address[i]);
+// 	}
+// }
+
+
+template<class T, class V>
+void iter(T* address, const std::size_t length, V(*function)(const T& element))
+{
+	for(std::size_t i = 0; i < length; i++)
+	{
+		function(address[i]);
+	}
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "iter.hpp"
+
+
+// template<class A> 
+// void print(const A& element)
+// {
+// 	std::cout << element << std::endl;
+// }
+
+// int main()
+// {
+// 	int a[] = {1, 2, 3, 4 ,5};
+// 	::iter(a, 5, print<int>);
+// 	return 0;
+// }
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<const Awesome> );
+
+  return 0;
+}
