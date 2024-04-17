81fb211cb6ab972a10945ff60af3014836671032
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-04-17 20:19:36 +0200

    just: more testes to main

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <iostream>
 
-int main()
-{
 
+
+//test that try to find 1 which exist
+void test1()
+{
 	std::vector<int> myVector(10, 1);
 	std::vector<int>::iterator myIterator;
 	try 
@@ -13,9 +15,60 @@ int main()
 	}
 	catch (std::exception &e)
 	{
-		std::cerr << "Something bad happpend" << std::endl;
-		return (1);
+		std::cerr << e.what() << std::endl;
+		return;
 	}
 	std::cout << *myIterator <<std::endl;
+}
+
+//test that try to find 2 which does not exist
+void test2()
+{
+	std::vector<int> myVector(10, 1);
+	std::vector<int>::iterator myIterator;
+	try 
+	{
+		myIterator = ::easyfind(myVector, 2);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return;
+	}
+	std::cout << *myIterator <<std::endl;
+}
+
+//test that try to find pushback value which does not exist
+void test3()
+{
+	std::vector<int> myVector(10, 1);
+	myVector.push_back(78);
+	myVector.push_back(17);
+	std::vector<int>::iterator myIterator;
+	try 
+	{
+		myIterator = ::easyfind(myVector, 78);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return;
+	}
+	std::cout << *myIterator <<std::endl;
+}
+
+int main()
+{
+	try 
+	{
+		test1();
+		test2();
+		test3();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Something bad happpend" << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
