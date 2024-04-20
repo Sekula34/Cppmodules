edcc9c6b7db5f39cd7528c91c607ecdc6f8f33d8
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-04-20 20:20:50 +0200

    Unfinished bussines

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -25,16 +25,30 @@ void BitcoinExchange::_openInputFile()
 {
 	_inFile.open(_inputFileName.c_str(), std::fstream::in);
 	if(_inFile.fail())
-		throw std::runtime_error("Opening file failed");
-	std::cout << "File opened succesfully" << std::endl;
+		throw std::runtime_error("Opening input file failed");
+}
+
+void BitcoinExchange::_fillMap(void)
+{
+	const std::string dataFileName("data.csv");
+	std::ifstream dataFile;
+
+	dataFile.open(dataFileName.c_str(), std::fstream::in);
+	if (dataFile.fail())
+	{
+		std::cerr<<"Canot open/find file in current folder with name " << dataFileName << std::endl;
+		throw std::runtime_error("Open failed");
+	}
+	std::cout <<"database opened" << std::endl;
 }
 BitcoinExchange::BitcoinExchange(std::string inputFileName):
 _inputFileName(inputFileName)
 {
 	_openInputFile();
+	_fillMap();
 }
 
 BitcoinExchange::~BitcoinExchange()
 {
-
+	_inFile.close();
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,6 +1,8 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
+#include <ctime>
 #include <fstream>
+#include <map>
 # include <string>
 
 class BitcoinExchange
@@ -9,12 +11,14 @@ class BitcoinExchange
 
 		const std::string _inputFileName;
 		std::ifstream _inFile;
+		std::map<std::time_t, double> _dataBaseMap;
 
 		//orthodox1
 		BitcoinExchange(void);
 		BitcoinExchange& operator=(const BitcoinExchange& source);
 		
 		void _openInputFile(void);
+		void _fillMap(void);
 
 	public:
 		//orthodox2
