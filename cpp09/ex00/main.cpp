3bd34f3429dbf44728e8049d87a9c44bc0954c1e
Filip Seles <fseles@c2r5p7.42vienna.com>
2024-04-20 19:09:12 +0200

    feat: input checker

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,6 +1,35 @@
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 
-int main()
+
+//return name of inputFile
+//throw runtime error if number of arguments is different than 2
+std::string getInputFileName(int argc, char** argv)
 {
+	std::string fileName;
+	if(argc != 2)
+		throw std::runtime_error("Program need only input file as agument");
+	fileName = argv[1];
+	if(fileName.empty())
+		throw std::runtime_error("File name is empty");
+	return (fileName);
+}
+
+
+int main(int argc, char** argv)
+{
+	try 
+	{
+		std::string inputFile;
+		std::cout<<"Hello" << std::endl;
+		inputFile = getInputFileName(argc, argv);
+		std::cout <<"Input file name is :[" << inputFile <<"]"<<std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
 	return 0;
 }
